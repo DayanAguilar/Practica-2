0c0370ae4cffb259c6da9c087c8027f50e53509e
DayanAguilar <dayan.aguilar.alcocer@gmail.com>
2024-10-10 23:37:28 -0400

    Unit tests added for get_opponent from utils.py in test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -1,12 +1,16 @@
 import pytest
-from utils import traduction_move
+from utils import traduction_move, get_opponent, BLACK, WHITE
 
-class TestUtils():
+class TestUtils:
 
     def test_traduction_move_valid(self):
-        assert(traduction_move("D1 SE"), (0, 3, 'SE'))
+        assert traduction_move("D1 SE") == (0, 3, 'SE')
 
     def test_traduction_move_invalid_length(self):
-        assert(traduction_move("D1SE"), "El movimiento debe contener una posición y una dirección (ej. 'D1 SE').")
+        assert traduction_move("D1SE") == "El movimiento debe contener una posición y una dirección (ej. 'D1 SE')."
 
-   
+    def test_get_white_opponent(self):
+        assert get_opponent(BLACK) == WHITE
+        
+    def test_get_black_opponent(self):
+        assert get_opponent(WHITE) == BLACK
