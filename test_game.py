eaa21f37be2cd7d0ca8d352481ae8d385d57df1b
“mateopoquechoque” <mateo.poquechoque@ucb.edu.bo>
2024-10-16 18:45:15 -0400

    unit test added for display_board from game.py in test_game.py

--- test_game.py
+++ test_game.py
@@ -14,3 +14,23 @@ class TestGame:
         ]
         assert board == expected_board
 
+    def test_display_board(self, capfd):
+        board = [
+            ['B', None, None, 'W'],
+            [None, 'B', 'W', None],
+            [None, 'W', 'B', None],
+            ['W', None, None, 'B']
+        ]
+        display_board(board)
+        captured = capfd.readouterr()
+        expected_output = (
+            '   A   B   C   D\n'
+            '1  B |   |   | W\n'
+            '  ---|---|---|---\n'
+            '2    | B | W |  \n'
+            '  ---|---|---|---\n'
+            '3    | W | B |  \n'
+            '  ---|---|---|---\n'
+            '4  W |   |   | B\n'
+        )
+        assert captured.out == expected_output
