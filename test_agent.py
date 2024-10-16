64af6d7c24e71106c089e1cc1ddb4ef5714d7c23
Oswi <edwin.roman@ucb.edu.bo>
2024-10-16 15:48:17 -0200

    Unit tests added for terminal_test from agent.py in the test_agent.py

--- test_agent.py
+++ test_agent.py
@@ -1,4 +1,5 @@
 from agent import is_winning_or_creates_special_case
+from agent import terminal_test
 
 class TestAgent:
 
@@ -42,3 +43,157 @@ class TestAgent:
         player = 'B'
         assert is_winning_or_creates_special_case(temp_board, player) == True
 
+    def test_terminal_test_first_row_win_B(self):
+        temp_board = [
+            ['B', 'B', 'B', 'B'],
+            ['', '', '', ''],
+            ['W', 'W', '', ''],
+            ['', '', '', '']
+        ]
+        player = 'B'
+        assert terminal_test(temp_board, player) == True
+
+    def test_terminal_test_second_row_win_B(self):
+        temp_board = [
+            ['', '', '', ''],
+            ['B', 'B', 'B', 'B'],
+            ['W', 'W', '', ''],
+            ['', '', '', '']
+        ]
+        player = 'B'
+        assert terminal_test(temp_board, player) == True
+
+    def test_terminal_test_third_row_win_W(self):
+        temp_board = [
+            ['B', 'B', '', ''],
+            ['', '', '', ''],
+            ['W', 'W', 'W', 'W'],
+            ['', '', '', '']
+        ]
+        player = 'W'
+        assert terminal_test(temp_board, player) == True
+
+    def test_terminal_test_fourth_row_win_W(self):
+        temp_board = [
+            ['B', 'B', '', ''],
+            ['', '', '', ''],
+            ['W', 'W', '', ''],
+            ['W', 'W', 'W', 'W']
+        ]
+        player = 'W'
+        assert terminal_test(temp_board, player) == True
+
+    def test_terminal_test_first_column_win_B(self):
+        temp_board = [
+            ['B', '', '', ''],
+            ['B', '', '', ''],
+            ['B', 'W', '', ''],
+            ['B', '', '', '']
+        ]
+        player = 'B'
+        assert terminal_test(temp_board, player) == True
+
+    def test_terminal_test_second_column_win_W(self):
+        temp_board = [
+            ['B', 'W', '', ''],
+            ['', 'W', '', ''],
+            ['B', 'W', '', ''],
+            ['', 'W', '', '']
+        ]
+        player = 'W'
+        assert terminal_test(temp_board, player) == True
+
+    def test_terminal_test_third_column_win_W(self):
+        temp_board = [
+            ['', '', 'W', ''],
+            ['B', '', 'W', ''],
+            ['B', '', 'W', ''],
+            ['', '', 'W', '']
+        ]
+        player = 'W'
+        assert terminal_test(temp_board, player) == True
+
+    def test_terminal_test_fourth_column_win_B(self):
+        temp_board = [
+            ['B', '', '', 'B'],
+            ['W', '', '', 'B'],
+            ['', 'W', '', 'B'],
+            ['', '', '', 'B']
+        ]
+        player = 'B'
+        assert terminal_test(temp_board, player) == True
+
+    def test_terminal_test_no_win(self):
+        temp_board = [
+            ['B', 'B', '', ''],
+            ['W', 'W', '', ''],
+            ['B', '', 'W', ''],
+            ['', '', '', '']
+        ]
+        player = 'B'
+        assert terminal_test(temp_board, player) == False
+
+    def test_terminal_test_incomplete_row(self):
+        temp_board = [
+            ['B', 'B', 'B', ''],
+            ['W', '', '', ''],
+            ['W', 'W', 'B', ''],
+            ['', '', '', '']
+        ]
+        player = 'B'
+        assert terminal_test(temp_board, player) == False
+
+    def test_terminal_test_incomplete_column(self):
+        temp_board = [
+            ['B', '', '', ''],
+            ['B', 'W', '', ''],
+            ['', '', 'B', ''],
+            ['', '', '', '']
+        ]
+        player = 'B'
+        assert terminal_test(temp_board, player) == False
+
+    def test_terminal_test_three_B(self):
+        temp_board = [
+            ['B', 'B', 'B', ''],
+            ['W', '', '', ''],
+            ['W', '', '', ''],
+            ['', '', '', '']
+        ]
+        player = 'B'
+        assert terminal_test(temp_board, player) == False
+
+    def test_terminal_test_empty_board(self):
+        temp_board = [
+            ['', '', '', ''],
+            ['', '', '', ''],
+            ['', '', '', ''],
+            ['', '', '', '']
+        ]
+        player = 'B'
+        assert terminal_test(temp_board, player) == False
+
+    def test_terminal_test_not_enough_W(self):
+        temp_board = [
+            ['B', 'W', '', ''],
+            ['W', 'W', '', ''],
+            ['', 'W', '', ''],
+            ['', '', '', '']
+        ]
+        player = 'W'
+        assert terminal_test(temp_board, player) == False
+
+    def test_terminal_test_max_B_W(self):
+        temp_board = [
+            ['B', 'B', '', ''],
+            ['B', 'W', 'W', ''],
+            ['W', 'W', 'B', ''],
+            ['B', '', '', 'W']
+        ]
+        player = 'B'
+        assert terminal_test(temp_board, player) == False
+
+    
+
+
+   
